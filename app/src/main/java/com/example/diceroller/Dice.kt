e2728266eb629adc294c51ecf9f1db2a4c415082
tientu2003 <117565347+tientu2003@users.noreply.github.com>
2024-10-05 10:42:40 +0700

    If else statements

--- app/src/main/java/com/example/diceroller/Dice.kt
+++ app/src/main/java/com/example/diceroller/Dice.kt
@@ -1,6 +1,21 @@
 package com.example.diceroller
 
-class Dice(private val numSides: Int) {
+fun main(){
+    val myFirstDice = Dice(6)
+    val rollResult = myFirstDice.roll()
+    val luckyNumber = 4
+    when (rollResult) {
+        luckyNumber -> println("You win!")
+        1 -> println("So sorry! You rolled a 1. Try again!")
+        2 -> println("Sadly, you rolled a 2. Try again!")
+        3 -> println("Unfortunately, you rolled a 3. Try again!")
+        5 -> println("Don't cry! You rolled a 5. Try again!")
+        else -> println("Apologies! You rolled a 6. Try again!")
+    }
+
+}
+
+class Dice(val numSides: Int) {
     fun roll() : Int {
         return (1..numSides).random()
     }
